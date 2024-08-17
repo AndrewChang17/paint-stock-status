0955b5005cd17a049fc817bb58ce884d4a1d4e7a
andrewchang <andrewchang17@hotmail.com>
2024-08-16 17:22:33 -0700

    changed postgres database host to Neon

--- server/index.js
+++ server/index.js
@@ -1,30 +1,30 @@
 const express = require("express");
+const postgres = require('postgres');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const index = express();
 const port = process.env.PORT || 3001;
 index.use(cors())
 index.use(bodyParser.json()) // for parsing application/json
-const { createClient } = require('@supabase/supabase-js');
+// const { createClient } = require('@supabase/supabase-js');
 
 require('dotenv').config()
 
-// const pgp = require('pg-promise')(/* options */)
-// const db = pgp(process.env.ENV === 'prod' ? process.env.INTERNAL : process.env.EXTERNAL) /*'postgres://username:password@host:port/database'*/
+const sql = postgres(process.env.ENV === 'prod' ? process.env.DATABASE_URL : process.env.DATABASE_URL_DEV) /*'postgres://username:password@host:port/database'*/
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+// const supabaseUrl = process.env.SUPABASE_URL
+// const supabaseKey = process.env.SUPABASE_KEY
+// const supabase = createClient(supabaseUrl, supabaseKey)
 
 
 
 // get all paints
-index.get('/api/paints', async (req, res) => {
+index.get('/api/paints', async (_, res) => {
     try {
-        // const resp = await db.manyOrNone("SELECT * FROM paints ORDER BY id")
-        // res.send(resp)
+        const resp = await sql`SELECT * FROM paints ORDER BY id`
+        res.send(resp)
 
-        const { data, error } = await supabase
+        /*const { data, error } = await supabase
             .from('paints')
             .select()
             .order('id', { ascending: true })
@@ -33,7 +33,7 @@ index.get('/api/paints', async (req, res) => {
         }
         if (data) {
             res.send(data)
-        }
+        }*/
     } catch (error) {
         console.error(error)
     }
@@ -44,10 +44,10 @@ index.patch('/api/paints/:id', async (req, res) => {
     try {
         const { id } = req.params
         const { available, low, out_of_stock } = req.body
-        // const resp = await db.none("UPDATE paints SET available = $1, low = $2, out_of_stock = $3 WHERE id = $4", [available, low, out_of_stock, id])
-        // res.send(resp)
+        const resp = await sql`UPDATE paints SET available = ${ available }, low = ${ low }, out_of_stock = ${ out_of_stock } WHERE id = ${ id }`
+        res.send(resp)
 
-        const { data, error } = await supabase
+        /*const { data, error } = await supabase
             .from('paints')
             .update({ available, low, out_of_stock })
             .eq('id', id)
@@ -56,7 +56,7 @@ index.patch('/api/paints/:id', async (req, res) => {
         }
         if (data) {
             res.send(data)
-        }
+        }*/
     } catch (error) {
         console.error(error)
     }
